223587ce82526cde78a16c08651480d02d4d2e7d
omer <oaksu.me@gmail.com>
2021-06-13 21:13:21 +0300

    animation

--- landmark/View/Landmark/LandmarkList.swift
+++ landmark/View/Landmark/LandmarkList.swift
@@ -38,5 +38,6 @@ struct LandmarkList: View {
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkList()
+            .environmentObject(ModelData())
     }
 }
