a9c5fb3a2cb2161f6351a86c8760d6a31c591b89
omer <oaksu.me@gmail.com>
2021-06-13 15:27:01 +0300

    handling user input

---
+++ landmark/View/Landmark/LandmarkList.swift
@@ -0,0 +1,42 @@
+//
+//  LandmarkList.swift
+//  landmark
+//
+//  Created by Ömer Aksu on 13.06.2021.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Show Favorites")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Dağlar Bayırlar")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
